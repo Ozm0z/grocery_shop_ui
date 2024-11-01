41b8948c9c1dd89fd779590967f69122602a7018
Özgün Can Erdoğan <ozgungezi@gmail.com>
2024-11-01 22:43:38 +0300

    string düzenlemelri
    ufak düzenleme

--- lib/product/language_items.dart
+++ lib/product/language_items.dart
@@ -4,7 +4,5 @@ class LanguageItems {
   final String mainTitle = 'We deliver grocery at your doorstep';
   final String subtitle = 'Groceer gives you fresh vegetables and fruits,\n Order fresh items from groceer';
   final String getStarted = 'Get started';
-  final String chickenLeg = '';
-  final String shoppingCart = '';
-  final String waterBottle = ' ';
+  final String hello = 'Hello!';
 }
